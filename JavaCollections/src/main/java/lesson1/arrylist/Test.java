546f77c9090eab7fec9c2bc71f8ca3049c8d023b
Alexander Kanishevskii <a.kanishevskii@gmail.com>
2020-07-23 23:15:03 +0300

    add log4j2 whis xml property's

--- JavaCollections/src/main/java/lesson1/arrylist/Test.java
+++ JavaCollections/src/main/java/lesson1/arrylist/Test.java
@@ -1,4 +1,20 @@
 package lesson1.arrylist;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+
 public class Test {
+    private static final Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        ArrayList <Integer> arrayList = new ArrayList();
+        for ( int i = 0; i <100000;i++){
+            arrayList.add(i);
+        }
+        logger.info("d" + "d");
+        logger.info("dsa");
+        logger.warn("sds");
+        logger.debug("da");
+    }
 }
