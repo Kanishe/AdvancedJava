714092ede8fe6557c044d32ad52d3847e879f21d
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-08-26 20:44:36 +0300

    add method compareTo

--- JavaCollections/src/main/java/lesson13interfacecomparator/Test.java
+++ JavaCollections/src/main/java/lesson13interfacecomparator/Test.java
@@ -10,18 +10,34 @@ public class Test {
         addElements(peopleList);
         addElements(peopleSet);
 
+        Collections.sort(peopleList);
+
+        System.out.println(peopleList);
+        System.out.println(peopleSet);
+
     }
     public static void addElements(Collection collection){
-        collection.add();
+        collection.add(new Person(1123,"Pablo"));
+        collection.add(new Person(12,"Kody"));
+        collection.add(new Person(92,"Fred"));
+        collection.add(new Person(1000,"Dracula"));
     }
 }
-class Person{
-    public int age;
-    public String name;
+class Person implements Comparable <Person>{
+    private int id;
+    private String name;
 
-    public Person(int age, String name) {
-        this.age = age;
+    public Person(int id, String name) {
+        this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 
     @Override
@@ -31,16 +47,33 @@ class Person{
 
         Person person = (Person) o;
 
-        if (age != person.age) return false;
+        if (id != person.id) return false;
         return name != null ? name.equals(person.name) : person.name == null;
     }
 
     @Override
     public int hashCode() {
-        int result = age;
+        int result = id;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.id > o.getId()){
+            return 1;
+        }
+        else if(this.id<o.getId()){
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 }
-//fix
 
