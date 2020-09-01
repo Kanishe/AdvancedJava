fe937682485386914f7821aec2141e22568ad36c
Alexander Kanishevskii <a.kan4988@gmail.com>
2020-09-01 21:11:28 +0300

    add object into LinkedList with Queue interface

--- JavaCollections/src/main/java/lesson14queue/View.java
+++ JavaCollections/src/main/java/lesson14queue/View.java
@@ -1,5 +1,9 @@
 package lesson14queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class View {
     public static void main(String[] args) {
         Person person1 = new Person("Goll",1);
@@ -8,5 +12,19 @@ public class View {
         Person person4 = new Person("Nell",4);
         Person person5 = new Person("Mopaaaer",5);
         Person person6 = new Person("AED",6);
+
+        Queue<Person> peopleLinkedList = new LinkedList<>();
+        //add person in LinkedList
+        peopleLinkedList.add(person5);
+        peopleLinkedList.add(person6);
+        peopleLinkedList.add(person3);
+        peopleLinkedList.add(person4);
+        peopleLinkedList.add(person1);
+        peopleLinkedList.add(person2);
+
+        for (Person person : peopleLinkedList){
+
+            System.out.println(person);
+        }
     }
 }
