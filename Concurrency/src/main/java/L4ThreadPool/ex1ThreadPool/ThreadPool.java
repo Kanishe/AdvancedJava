691505e3ab600cb0153f5f4abb3616b81b113414
Kanishe <a.kanishevskii@gmail.com>
2022-06-12 22:40:47 +0300

    Concurrence Lesson4 ThreadPool

---
+++ Concurrency/src/main/java/L4ThreadPool/ex1ThreadPool/ThreadPool.java
@@ -0,0 +1,43 @@
+package L4ThreadPool.ex1ThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPool {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(new Worker(i));
+        }
+        executorService.shutdown();
+        System.out.println("All tasks submitted");
+    
+    
+        try {
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Worker implements Runnable {
+    private int id;
+    
+    public Worker(int id){
+        this.id = id;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Work " + id + " is completed");
+
+        
+    }
+}
